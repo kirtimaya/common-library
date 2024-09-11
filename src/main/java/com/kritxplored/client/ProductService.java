0fa96e2a37d5dd6e3138b1e7891fcc84db4886fc
kirtimayaswain <kirtimaya@github.com>
2024-09-11 18:01:18 +0530

    added ProductService

---
+++ src/main/java/com/kritxplored/client/ProductService.java
@@ -0,0 +1,14 @@
+package com.kritxplored.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "PRODUCTSERVICE/product")
+public interface ProductService {
+
+    @PutMapping("/reduceQuantity/{id}")
+    public ResponseEntity<Void> reduceQuantity(@PathVariable("id") long productId, @RequestParam long quantity);
+}
